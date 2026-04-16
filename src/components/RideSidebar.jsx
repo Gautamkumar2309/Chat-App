8c2a49021cd072e77d0787d942b4534acfbf1356
Gautam <gautamkumarofficial2309@gmail.com>
2026-04-16 18:44:49 +0530

    first commit

---
+++ src/components/RideSidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RideSidebar = () => {
+  return (
+    <div>
+      <h1>RightSideBar</h1>
+    </div>
+  )
+}
+
+export default RideSidebar
---
+++ src/pages/HomePages.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HomePages = () => {
+  return (
+    <div>
+      <div className='border w-full h-screen sm:px-[15%] sm:py-[5%]'>
+        
+      </div>
+    </div>
+  )
+}
+
+export default HomePages

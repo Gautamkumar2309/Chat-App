9528b9341799791df287f8f159550a964f1abfe3
Gautam <gautamkumarofficial2309@gmail.com>
2026-04-16 19:35:35 +0530

    Design the home page and sidebar

--- src/pages/HomePages.jsx
+++ src/pages/HomePages.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Sidebar from '../components/Sidebar'
 import ChatContainer from '../components/ChatContainer'
 import RightSidebar from '../components/RightSidebar'
 
 const HomePages = () => {
+
+      const [selectedUser, setSelectedUser] = useState(false)
+
+
   return (
     <div className='border w-full h-screen sm:px-[15%] sm:py-[5%]'>
-      <div >
+      <div className={`backdrop-blur-xl border-2 border-gray-600 rounded-2xl overflow-hidden grid h-[100%] grid grid-cols-l relative ${selectedUser ? 
+      'md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]' : 'md:grid-cols-2'}`}>
         <Sidebar />
         <ChatContainer />
         <RightSidebar />

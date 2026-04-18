6e09e3fa536a1d6931a015eae30da43b4b2792bd
Gautam <gautamkumarofficial2309@gmail.com>
2026-04-18 11:28:00 +0530

    imprt the utils time

--- src/components/ChatContainer.jsx
+++ src/components/ChatContainer.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef } from 'react'
 import assets, { messagesDummyData } from '../assets/assets'
+import { formatMessageTime } from '../lib/utils'
 
 const ChatContainer = ({selectedUser, setSelectedUser}) => {
 
@@ -41,7 +42,7 @@ const ChatContainer = ({selectedUser, setSelectedUser}) => {
                 avatar_icon : assets.profile_martin} alt="" className='w-7
                 rounded-full' />
 
-                <p className='text grey-500'>{msg.createdAt}</p>
+                <p className='text grey-500'>{formatMessageTime (msg.createdAt)}</p>
             </div>
           </div>
         ))}

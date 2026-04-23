edd8db632a59a9403f6c459024478ab8e5e277bf
Gautam <gautamkumarofficial2309@gmail.com>
2026-04-23 12:02:10 +0530

    add checkAuth functionalities

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -50,3 +50,8 @@ export const login = async (req, res) =>{
         res.json({sucess: false, message: error.message})
     }
 }
+
+//controler to check if user is authenticated
+export const checkAuth = (req, res)=>{
+    res.json({sucess: true, user: req.user});
+}

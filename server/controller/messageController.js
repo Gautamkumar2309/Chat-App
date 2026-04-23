9d5373b403856a897adc5c2036481808d50ad3df
Gautam <gautamkumarofficial2309@gmail.com>
2026-04-23 19:01:48 +0530

    add getMessages API for chat functionality

--- server/controller/messageController.js
+++ server/controller/messageController.js
@@ -23,3 +23,25 @@ export const getUserForSidebar = async (req, res) => {
         res.json({success: false, message: error.message})
     }
 }
+
+//get all message for selectede user
+
+export const getMessages = async (req, res) =>{
+    try{
+        const {id: selectedUserId } = req.params;
+        const myId = req.user._id;
+
+        const messages = await Message.find({
+            $or: [
+                { senderId: myId, receiverId: selectedUserId },
+                { senderId: selectedUserId, receiverId: myId } ,
+            ]
+        })
+        await Message.updateMany({ senderId: selectedUserId, receiverId: myId }, {seen: true});
+        res.json({success: true, messages})
+
+    }catch(error){
+        console.log(error.message);
+        res.json({success: false, message: error.message})
+    }
+}

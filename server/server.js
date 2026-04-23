fbbf44db516ab5add9e00bb048d819d27ae4a377
Gautam <gautamkumarofficial2309@gmail.com>
2026-04-23 10:29:37 +0530

    correct the server.js code

--- server/server.js
+++ server/server.js
@@ -1,45 +1,22 @@
-// import express from "express";
-// import "dotenv/config";
-// import cors from "cors";
-// import http from "http";
-// import { connectDB } from "./lib/db.js";
-
-// dotenv.config();
-
-// const app = express();
-// const server = http.createServer(app)
-
-// app.use(express.json({limit: "4mb"}));
-// app.use(cors());
-
-// app.use("/api/status", (req,res)=> res.send("Server is live"));
-
-// //conect to mongdb
-// await connectDB();
-
-// const PORT = process.env.PORT || 5000;
-// server.listen(PORT, ()=> console.log("Server is running on port: " + PORT));
 import express from "express";
+import "dotenv/config";
 import dotenv from "dotenv";
 import cors from "cors";
 import http from "http";
 import { connectDB } from "./lib/db.js";
 
-dotenv.config(); // 👈 important
+dotenv.config();
 
 const app = express();
-const server = http.createServer(app);
+const server = http.createServer(app)
 
-app.use(express.json({ limit: "4mb" }));
+app.use(express.json({limit: "4mb"}));
 app.use(cors());
 
-app.use("/api/status", (req, res) => res.send("Server is live"));
+app.use("/api/status", (req,res)=> res.send("Server is live"));
 
-// connect to mongodb
+//conect to mongdb
 await connectDB();
 
 const PORT = process.env.PORT || 5000;
-
-server.listen(PORT, () => {
-  console.log("Server is running on port: " + PORT);
-});
+server.listen(PORT, ()=> console.log("Server is running on port: " + PORT));

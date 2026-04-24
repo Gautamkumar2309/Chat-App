f9693be31d8cf74f54e79663b9a1aa1163922ce4
Gautam <gautamkumarofficial2309@gmail.com>
2026-04-24 23:31:34 +0530

    update and config the App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,20 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import React, { useContext } from 'react'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import HomePages from './pages/HomePages'
 import LoginPage from './pages/LoginPage'
 import ProfilePage from './pages/ProfilePage'
 import { Toaster } from "react-hot-toast"
+import { AuthContext } from '../context/AuthContext'
 
 const App = () => {
+  const { authUser } = useContext(AuthContext)
   return (
     <div className="bg-[url('./src/assets/bgImage.svg')] bg-contain">
       <Toaster/>
       <Routes>
-        <Route path='/' element={<HomePages />} />
-        <Route path='/Login' element={<LoginPage />} />
-        <Route path='/Profile' element={<ProfilePage />} />
+        <Route path='/' element={authUser ? <HomePage />: <Navigate to="/login"/>}/>
+        <Route path='/login' element={!authUser ? <LoginPage /> : <Navigate to="/l"/>}/>
+        <Route path='/Profile' element={authUser ? <ProfilePage /> :<Navigate to="/login"/>} />
       </Routes>
     </div>
   )

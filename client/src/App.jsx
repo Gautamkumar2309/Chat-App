94522e56b4d937bb05c9785caa3a50382182059d
Gautam <gautamkumarofficial2309@gmail.com>
2026-04-25 01:17:39 +0530

    correct the minor error

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,8 +12,8 @@ const App = () => {
     <div className="bg-[url('./src/assets/bgImage.svg')] bg-contain">
       <Toaster/>
       <Routes>
-        <Route path='/' element={authUser ? <HomePage />: <Navigate to="/login"/>}/>
-        <Route path='/login' element={!authUser ? <LoginPage /> : <Navigate to="/l"/>}/>
+        <Route path='/' element={authUser ? <HomePages />: <Navigate to="/login"/>}/>
+        <Route path='/login' element={!authUser ? <LoginPage /> : <Navigate to="/"/>}/>
         <Route path='/Profile' element={authUser ? <ProfilePage /> :<Navigate to="/login"/>} />
       </Routes>
     </div>
